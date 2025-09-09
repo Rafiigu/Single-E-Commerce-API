754f5b09aa9c3d3435120f5bc88890b121b0c7eb
Jayaku Briliantio <jayakubriliantio@gmail.com>
2025-09-09 17:34:30 +0700

    feat: add new endpoint group: user

---
+++ src/user/handlers.ts
@@ -0,0 +1,6 @@
+// get list of users
+// pagination, search by name or email, filter by status (please referenced to listAdminsQuerySchema)
+
+// block user: set user status to blocked
+
+// unblock user: set user status to verified
