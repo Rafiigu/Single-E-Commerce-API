2bce8761932160228fcc1d9c0d9efc0c18f361f9
Rafiigu <timothys.tobing23@.com>
2026-01-16 13:35:29 +0700

    Feat: Deleted Original Field in ProductImages Table

---
+++ prisma/migrations/20260116063413_deleted_product_images_original_nam/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `original` on the `productimages` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `productimages` DROP COLUMN `original`;
