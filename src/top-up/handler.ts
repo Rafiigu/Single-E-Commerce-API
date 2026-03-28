33cb03e7635700d01f40b9724a1b5b9b1231032b
Rafiigu <timothys.tobing23@.com>
2026-03-28 21:02:08 +0700

    Feat: Updated Top Up Table and Handler

--- src/top-up/handler.ts
+++ src/top-up/handler.ts
@@ -37,7 +37,7 @@ export const listTopUpsHandler: HandlerWithDeps = ({ prisma }) =>
           omit: { paymentAccountId: true },
           orderBy: { createdAt: "desc" },
           include: {
-            PaymentAccount: {
+            paymentAccount: {
               select: {
                 id: true,
                 accountHolderName: true,
@@ -86,7 +86,7 @@ export const getTopUpHandler: HandlerWithDeps = ({ prisma }) =>
           where: { id },
           omit: { paymentAccountId: true },
           include: {
-            PaymentAccount: {
+            paymentAccount: {
               select: {
                 id: true,
                 accountHolderName: true,
